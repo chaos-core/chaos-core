c4664503e626c6f216312fc282ecf8c318119b04
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-06-01 20:18:15 -0400

    add react router

--- lib/plugins/web-api/web-client/index.jsx
+++ lib/plugins/web-api/web-client/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import chaosPlugins from './chaos-plugins';
 
@@ -9,11 +10,25 @@ import tifaImg from "./tifa.jpg";
 
 const App = () => (
   <div className={"CoreApp"}>
-    <img src={tifaImg}/>
+    <img src={tifaImg} alt={"Logo"}/>
     Hello from React!
     {Object.entries(chaosPlugins).map(([pluginName, App]) => (
       <App key={pluginName}/>
     ))}
+
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <div>Route: /about</div>
+        </Route>
+        <Route path="/users">
+          <div>Route: /users</div>
+        </Route>
+        <Route path="/">
+          <div>Route: /</div>
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
