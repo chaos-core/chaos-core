e1f66b6a4c156f506cadafb03f7a4d16a24461e0
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-06-29 23:03:57 -0400

    allow for running config actions via web API

--- lib/core-plugin/web-client/plugins/config-action.jsx
+++ lib/core-plugin/web-client/plugins/config-action.jsx
@@ -1,14 +1,30 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
+import ChaosApiService from '../chaos-api-service.js';
+import {PluginContext} from './plugins-context.jsx';
+import {GuildContext} from '../guilds';
+
 import './config-action.scss';
 
 const ConfigAction = ({action}) => {
+  const {guild} = useContext(GuildContext);
+  const {plugin} = useContext(PluginContext);
+
   const [expanded, setExpanded] = useState(false);
+  const [argInputs, setInputs] = useState({});
+
+  const onSend = () => {
+    ChaosApiService.plugin(plugin.name).runAction(
+      action.name,
+      guild.id,
+      argInputs,
+    );
+  };
 
   return (
     <div className={'chaos-config-action'}>
@@ -19,10 +35,20 @@ const ConfigAction = ({action}) => {
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <div className={'args'}>
-            {action.args.map((arg) => <ArgInput key={arg.name} arg={arg}/>)}
+            {action.args.map((arg) => (
+              <ArgInput
+                key={arg.name}
+                arg={arg}
+                value={argInputs[arg.name] || ''}
+                onChange={(event) => setInputs({
+                  ...argInputs,
+                  [arg.name]: event.target.value,
+                })}
+              />
+            ))}
           </div>
           <div className={'actions'}>
-            <Button variant="outlined" color="default">Send</Button>
+            <Button variant="outlined" color="default" onClick={onSend}>Send</Button>
           </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
@@ -32,10 +58,18 @@ const ConfigAction = ({action}) => {
 
 export default ConfigAction;
 
-const ArgInput = ({arg}) => {
+const ArgInput = ({arg, value, onChange}) => {
+  let label = arg.name;
+  let helperText = arg.description;
+
+  if (arg.required) {
+    label += '*';
+    helperText = '(Required) ' + helperText;
+  }
+
   return (
     <div className={'arg'}>
-      <TextField id="standard-basic" fullWidth label={arg.name} helperText={arg.description}/>
+      <TextField id="standard-basic" fullWidth label={label} helperText={helperText} value={value} onChange={onChange}/>
     </div>
   );
 };
