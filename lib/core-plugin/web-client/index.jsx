69dcbe43e8ad6fe0f07f87a1932820fbc45a3cbb
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-07-19 14:42:03 -0400

    Set page title to bot name

--- lib/core-plugin/web-client/index.jsx
+++ lib/core-plugin/web-client/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ThemeProvider} from '@material-ui/core/styles';
 
+import { CoreApiClient } from './chaos-api-client.js';
 import appTheme from './app-theme.js';
 import AppRouter from './app-router.jsx';
 import {UserProvider} from './user';
@@ -11,6 +12,10 @@ import {GuildProvider} from './guilds';
 import './index.scss';
 
 const App = () => {
+  useEffect(() => {
+    CoreApiClient.getBotName().then((name) => document.title = name);
+  }, []);
+
   return (
     <ThemeProvider theme={appTheme}>
       <div className={'app'}>
